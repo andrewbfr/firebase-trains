facf80f386024753555851541c668e69d82c3140
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-31 18:01:22 -0600

    regular and periodic updates

---
+++ assets/javascript/train.js
@@ -0,0 +1,15 @@
+// maybe have a splash page that asks "is there anywhere you would like to go?" followed by an input field and another button that directs to the train schedule itself
+//
+
+
+
+ // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBRfBqVGuVxWCHz5wbJyULjhIGUZ10xBMU",
+    authDomain: "train-timers-57799.firebaseapp.com",
+    databaseURL: "https://train-timers-57799.firebaseio.com",
+    projectId: "train-timers-57799",
+    storageBucket: "train-timers-57799.appspot.com",
+    messagingSenderId: "936426178638"
+  };
+  firebase.initializeApp(config);
