cc1247162bbf2eb2ba61e587eeea5cb115718856
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-11-02 16:41:35 -0600

    implement successful firebase storage

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,7 +1,4 @@
 // maybe have a splash page that asks "is there anywhere you would like to go?" followed by an input field and another button that directs to the train schedule itself
-//
-
-
 
  // Initialize Firebase
   var config = {
@@ -13,3 +10,30 @@
     messagingSenderId: "936426178638"
   };
   firebase.initializeApp(config);
+
+
+  var database = firebase.database();
+
+    var userRef = database.ref("/new-train");
+
+     // Capture Button Click
+    $("#add-train").on("click", function() {
+      // Don't refresh the page!
+      event.preventDefault();
+
+
+      // Code in the logic for storing and retrieving the most recent user.
+      var train= $("#train-input").val().trim();
+      var destination = $("#destination-input").val().trim();
+      var military = $("#military-input").val().trim();
+      var interval = $("#interval-input").val().trim();
+
+      var data = {
+        title: train,
+        locale: destination,
+        schedule: military,
+        frequency: interval
+      };
+
+      userRef.push(data);
+    });
